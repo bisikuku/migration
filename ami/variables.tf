ab36931fc9b6294a9b1ba0835dde9b78fdc8c14b
bisikuku <adebisikuku@icloud.com>
2022-01-21 00:28:35 -0500

    Update variables.tf

--- ami/variables.tf
+++ ami/variables.tf
@@ -2,5 +2,5 @@ variable "account_id" {}
 variable "ami_name" {}
 variable "source_instance_id" {}
 variable "aws_region" {}
-variable "key_tela" {}
-variable "secret_tela" {}
+# variable "key_tela" {}
+# variable "secret_tela" {}
